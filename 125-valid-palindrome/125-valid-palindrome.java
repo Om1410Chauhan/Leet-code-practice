042aff5c317ea9120fb8463a10e78234eeb640b5
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-07-29 19:25:09 +0530

    Time: 5 ms (77.28%), Space: 43.8 MB (54.15%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        if(s==null){
+            return false;
+        }
         String ans=s.toLowerCase();
         String answer="";
-        for(int i=0;i<s.length();i++){
-           if(Character.isLetterOrDigit(ans.charAt(i))){
-               answer+=ans.charAt(i);
-           }else{
-               continue;
-           }
-        }
-        int i=0;
-        int j=answer.length()-1;
-        while(i<=j){
-            if(answer.charAt(i)!=answer.charAt(j)){
-                return false;
-            }else{
+         int i=0;
+        int j=ans.length()-1;
+        while(i<j){
+            while(i<j && !((ans.charAt(i)>='a' && ans.charAt(i)<='z')|| (ans.charAt(i)>='0'&& ans.charAt(i)<='9'))){
                 i++;
-                j--;
             }
+                        while(i<j && !((ans.charAt(j)>='a' && ans.charAt(j)<='z')|| (ans.charAt(j)>='0'&& ans.charAt(j)<='9'))){
+                            j--;
+                        }
+            if(ans.charAt(i)!=ans.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+            
+            
         }
         return true;
     }
