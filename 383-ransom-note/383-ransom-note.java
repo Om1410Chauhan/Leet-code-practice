9a641e7ed9f810307e9d4dfc3f036e996dc1eeb4
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-07-30 14:38:23 +0530

    Time: 3 ms (91.52%), Space: 46 MB (64.85%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] array=new int[26];
+        for(char ch:magazine.toCharArray()){
+            
+            array[ch-'a']++;
+        }
+        for(char ch:ransomNote.toCharArray()){
+            if(array[ch-'a']!=0){
+                array[ch-'a']--;
+            }else if(array[ch-'a']==0){
+                return false;
+            }
+        }
+    
+    return true;
+                     }
+}
