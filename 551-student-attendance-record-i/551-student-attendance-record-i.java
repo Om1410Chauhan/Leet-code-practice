d9b1bd3e491e02eadad092f2b7ab308f83cb1cb3
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-08-01 08:56:14 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (54.35%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.java
+++ 551-student-attendance-record-i/551-student-attendance-record-i.java
@@ -1,18 +1,8 @@
 class Solution {
     public boolean checkRecord(String s) {
-        int p=0,l=0,a=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='P'){
-                continue;
-            }else if(s.charAt(i)=='A' ){
-                a++;
-            }
-            else if(s.contains("LLL")){
-               return false;
-            }
+        if(s.indexOf("A")!=s.lastIndexOf("A") || s.contains("LLL")){
+          return false;
         }
-        if(a>=2)
-            return false;
         return true;
     }
 }
