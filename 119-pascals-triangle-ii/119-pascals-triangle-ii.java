2779192ead605cf1310600a6c6abb422aa1e4557
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-08-22 09:45:04 +0530

    Time: 2 ms (58.69%), Space: 41 MB (84.17%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+
+        List<List<Integer>> li=new ArrayList();
+        for(int i=1;i<=rowIndex+1;i++){
+            List<Integer>current=new ArrayList();
+            // j varies according to number of rows
+            for(int j=0;j<i;j++){
+                if(j==0 || j==i-1){
+                    current.add(1);
+                }else{
+                    // we get previous row from li and get j th and j-1 element beacuse we need j and j-1 elemenet
+                    // to build our new element 
+                    current.add(li.get(i-2).get(j)+li.get(i-2).get(j-1));
+                }
+             
+            } 
+               li.add(current);
+        }
+             return li.get(rowIndex);
+    }
+}
