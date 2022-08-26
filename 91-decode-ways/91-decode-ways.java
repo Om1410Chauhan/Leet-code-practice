d4e388fdbfb2014a210b8e21ff40cd18675c2a9a
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-08-26 15:23:04 +0530

    Time: 3 ms (42.02%), Space: 43.1 MB (12.06%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,7 +1,9 @@
 class Solution {
     public int numDecodings(String s) {
+        /*
       // using memoiazation
         int n=s.length();
+        // here dp array tells us the way to decode the character at index by how many times
         int dp[]=new int[n+1];
         int ways=numberOfWays(s,0,dp);
         return ways;
@@ -9,11 +11,12 @@ class Solution {
     private int numberOfWays(String s,int index,int[] dp){
         if(index==s.length()){
             return 1;
-        }
+        } 
       
         if(s.charAt(index)=='0'){
             return 0;
         }
+        
           if(index==s.length()-1){
             return 1;
         }
@@ -27,5 +30,29 @@ class Solution {
         }
         dp[index]=way1+way2;
         return way1+way2;
+        */
+        // using tabulation
+         int n=s.length();
+        // here every index of dp teels us ways to decode a string having length==index
+        
+        int dp[]=new int[n+1];
+        if(s.charAt(0)=='0' || s.length()==0){
+            return 0;
+        }
+        // here dp[0]=1 for the empty strings
+        dp[0]=1;
+        // and dp[1]=1 if it isn't==0
+        dp[1]=s.charAt(0)!=0?1:0;
+        for(int i=2;i<n+1;i++){
+            int first=Integer.valueOf(s.substring(i-1,i));
+            int second=Integer.valueOf(s.substring(i-2,i));
+            if(first>=1 && first<=9){
+                dp[i]+=dp[i-1];
+            }
+            if(second>=10 && second<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
     }
 }
