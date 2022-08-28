7cbd93d37fae67c5c1e74f097eb28ae885d7189f
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-08-28 16:37:11 +0530

    Time: 2716 ms (5.00%), Space: 220.4 MB (10.12%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int minCut(String s) {
+        /*
         if(s.length()==1){
             return 0;
         }
@@ -9,8 +10,8 @@ class Solution {
         for(int i=0;i<n;i++){
             dp[i]=-1;
         }
-        int mincut=f(0,dp,s);
-        return mincut-1;
+        int mincut=f(0,dp,s)-1;
+        return mincut;
     }
     private int f(int i,int[] dp,String s){
         if(i==s.length()){
@@ -32,7 +33,32 @@ class Solution {
         }
         return  dp[i]=min;
     }
-    private boolean isPalindrome(String s){
+    */
+  
+    
+        // using tabulation
+        
+        if(s.length()==1){
+            return 0;
+        }
+        int n=s.length();
+        int[] dp=new int[n+1];
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+           String temp="";
+           int min=Integer.MAX_VALUE;
+           for(int j=i;j<s.length();j++){
+             temp+=s.charAt(j);
+                if(isPalindrome(temp)){
+                  int cost=1+dp[j+1];
+                  min=Math.min(min,cost);
+                } 
+            }
+           dp[i]=min; 
+        }
+        return dp[0]-1;
+    }
+      private boolean isPalindrome(String s){
         int i=0;
         int j=s.length()-1;
         while(i<j){
