05b4b5c5a37cd4a78975c15a71dc3276021f97d7
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-08-31 13:16:32 +0530

    Time: 11 ms (10.34%), Space: 42.9 MB (13.88%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -1,22 +1,27 @@
 class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
        HashSet<String> set=new HashSet<>(wordDict);
-        return helper(s,0,set);
+        HashMap<Integer,List<String>> map=new HashMap<>();
+        return helper(s,0,set,map);
     }
-    private List<String> helper(String s,int start,HashSet<String> dict){
+    private List<String> helper(String s,int start,HashSet<String> dict,HashMap<Integer,List<String>> map){
         List<String> current=new ArrayList<>();
+        if(map.containsKey(start)){
+            return map.get(start);
+        }
         if(start==s.length()){
             current.add("");
         }
         for(int i=start+1;i<=s.length();i++){
             String prefix=s.substring(start,i);
             if(dict.contains(prefix)){
-                List<String> suffix=helper(s,i,dict);
+                List<String> suffix=helper(s,i,dict,map);
                for(String str: suffix){
                   current.add(prefix+ (str.equals("")?"":" ") +str);
                }
             }
         }
+        map.put(start,current);
         return current;
     }
 }
