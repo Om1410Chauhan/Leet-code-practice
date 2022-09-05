b5ed83429d8735c85947c03a78f6db15b5089cc5
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-09-05 16:23:19 +0530

    Time: 48 ms (92.67%), Space: 42.9 MB (72.41%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -1,4 +1,8 @@
+// memoiazation time complexity=o(N3)
+// space complexity=O(n2+n)
+/*
 class Solution {
+    
     public int maxCoins(int[] nums) {
        int n=nums.length;
         if(n==1){
@@ -17,11 +21,13 @@ class Solution {
         // for initiallising value 1 to array out of bound condition
         nCut[0]=1; // inserting 1 at -1 index
         nCut[n+1]=1;  // index 1 at n+1 index
+        // to copy nums array into nCut array using below function
         System.arraycopy(nums,0,nCut,1,n);
         int i=1; 
         int j=n;
         return memo(i,j,nCut,dp);
     }
+    
     private int memo(int i,int j,int[]nums,int[][]dp){
         // when array is empty
         if(i>j){
@@ -38,4 +44,32 @@ class Solution {
         }
         return dp[i][j]=maxi;
     }
-}
+    */
+    // using tabulation
+    class Solution {
+         public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] nCuts = new int[n + 2];         // creating a new array of size n+2
+        nCuts[0] = 1;                         // inserting 1 at the front  
+        nCuts[n + 1] = 1;                     //inserting 1 at the back
+        System.arraycopy(nums, 0, nCuts, 1, n);
+        
+        //int dp[][] = new int[n+1][n+1];
+       // return f(1,n,nCuts,dp);
+        int dp[][] = new int[n+2][n+2]; //for tabulation
+        
+        for(int i=n;i>=1;i--){
+            for(int j =1;j<=n;j++){
+                 if(i>j) continue;
+                 int maxi = Integer.MIN_VALUE;
+                 for(int ind =i;ind <= j;ind++){
+                     int cost = nCuts[i-1] * nCuts[ind] * nCuts[j+1] + dp[i][ind-1] + dp[ind+1][j];
+                     maxi= Math.max(cost,maxi);
+            
+                 }
+                 dp[i][j] =  maxi;
+            }
+        }
+        return dp[1][n];
+    }
+    }
