7dc945197222e14fcef881270969f327817bc630
Om Chauhan <109066042+Om1410Chauhan@users.noreply.github.com>
2022-09-06 18:50:22 +0530

    Time: 24 ms (5.89%), Space: 48.6 MB (18.95%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,26 @@
+class Solution {
+    // i this question we have to recognise a pattern
+    // the pattern is that for even numbers number of 1 is qual to n/2
+    // and for odd it is 1+n/2;
+    public int[] countBits(int n) {
+        int [] ans=new int[n+1];
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=helper(i,dp);
+        }
+        return ans;
+    }
+    private int helper(int n,int[] dp){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n%2==0){
+            return dp[n]=helper(n/2,dp);
+        }else{
+            return dp[n]=1+helper(n/2,dp);
+        }
+    }
+}
